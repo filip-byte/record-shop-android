7d0292890b925b8a9a73db6063dba1b4f268156d
filip-byte <ohboytoby@gmail.com>
2024-09-19 15:03:51 +0100

    feat: Add addNewAlbum() method to the MainActivityViewModel

--- app/src/main/java/com/example/recordshop/ui/mainactivity/MainActivityViewModel.java
+++ app/src/main/java/com/example/recordshop/ui/mainactivity/MainActivityViewModel.java
@@ -23,4 +23,8 @@ public class MainActivityViewModel extends AndroidViewModel {
     public MutableLiveData<List<Album>> getAlbums() {
         return albumRepository.getMutableLiveData();
     }
+
+    public void addNewAlbum(Album album) {
+        albumRepository.addAlbum(album);
+    }
 }
