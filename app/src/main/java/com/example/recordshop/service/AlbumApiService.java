d0edc871886f2e91a9b27891f07c9bdec62f4aa3
filip-byte <ohboytoby@gmail.com>
2024-09-19 10:57:58 +0100

    feat: Add AlbumApiService

---
+++ app/src/main/java/com/example/recordshop/service/AlbumApiService.java
@@ -0,0 +1,15 @@
+package com.example.recordshop.service;
+
+import com.example.recordshop.model.Album;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface AlbumApiService {
+
+    @GET("albums")
+    public Call<List<Album>> getAllAlbums();
+
+}
