9a30575252e2185d878bfdb817b49f1744ebca65
filip-byte <ohboytoby@gmail.com>
2024-09-19 10:59:12 +0100

    feat: Add MainActivityViewModel

---
+++ app/src/main/java/com/example/recordshop/ui/mainactivity/MainActivityViewModel.java
@@ -0,0 +1,26 @@
+package com.example.recordshop.ui.mainactivity;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.recordshop.model.Album;
+import com.example.recordshop.service.AlbumRepository;
+
+import java.util.List;
+
+public class MainActivityViewModel extends AndroidViewModel {
+
+    AlbumRepository albumRepository;
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        this.albumRepository = new AlbumRepository(application);
+    }
+
+    public MutableLiveData<List<Album>> getAlbums() {
+        return albumRepository.getMutableLiveData();
+    }
+}
