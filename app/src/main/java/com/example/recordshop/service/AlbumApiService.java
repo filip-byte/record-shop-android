bd2c0fba719db3c17933dc92bb1660a57b0e2ebc
filip-byte <ohboytoby@gmail.com>
2024-09-19 11:52:46 +0100

    feat: Add createAlbum() with @Body

--- app/src/main/java/com/example/recordshop/service/AlbumApiService.java
+++ app/src/main/java/com/example/recordshop/service/AlbumApiService.java
@@ -5,11 +5,16 @@ import com.example.recordshop.model.Album;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface AlbumApiService {
 
     @GET("albums")
     public Call<List<Album>> getAllAlbums();
+
+    @POST("albums")
+    public Call<Album> createAlbum(@Body Album album);
 
 }
